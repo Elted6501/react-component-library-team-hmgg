bdf44d33212c6a51b0af6d68d4e74403f47ed63f
cecexp <trabajos.cece@gmail.com>
2025-02-15 17:56:22 -0600

    My own first commit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,8 +4,21 @@ interface ButtonProps {
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button: React.FC<ButtonProps> = ({ label }) => {
+  return <button style={styles()}>{label}</button>;
 };
+
+function styles() {
+  return {
+    background: "#26B7ED",
+    color: "white",
+    border: "2px",
+    padding: "10px 20px",
+    fontSize: "16px",
+    fontWeight: "bold",
+    borderRadius: "5px",
+    cursor: "pointer",
+  };
+}
 
 export default Button;
