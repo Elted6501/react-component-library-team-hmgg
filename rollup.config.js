4504ddf2c2152c45efdae7cc5364813a59d469a9
Hector Ortiz <hector.i.ortiz@autozone.com>
2025-02-15 15:07:18 -0600

    First Commit to test library

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/esm/index.js",
+            format: "esm",
+
+        }, plugins: [
+            resolve(), commonjs(), typescript({tsconfig: "./tsconfig.json", compilerOptions: {declaration: true, declarationDir: "./dist/esm/types"}}) 
+        ]
+    },
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/cjs/index.js",
+            format: "cjs",
+
+        }, plugins: [
+            resolve(), commonjs(), typescript({tsconfig: "./tsconfig.json", compilerOptions: {declaration: true, declarationDir: "./dist/cjs/types"}}) 
+        ]
+    }
+]
